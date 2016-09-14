317860f4219f40023b1a0d1fb380985b6e4ea0c5
lromang <luis.roangarci@gmail.com>
2016-09-13 22:20:20 -0500

    backTrack

--- c_code/gen_optim.c
+++ c_code/gen_optim.c
@@ -96,8 +96,44 @@ double* hessCentralDiff(double (*func)(double*, int), double* x, double* p, int 
  * -------------------------------------
  */
 double phi(double (*func)(double*, int), double* x, double* p, double alpha, int length){
+  // phi(a) = f(x + ap)
   return func(vSum(x, vProd(p, alpha, length), length), length);
 }
+
+/* -------------------------------------
+ * Método auxiliar para búsqueda de línea.
+ * IN
+ * fun: Apuntador a una función que recibe
+ * un apuntador a un double y un entero.
+ * x: Apuntador a un vector sobre el cual
+ * se quiere aproximar el gradiente
+ * p: Una dirección de descenso.
+ * length: Longitud del vector.
+ * OUT
+ * res: Gradiente de fun evaluado en x.
+ * -------------------------------------
+ */
+double zoom(double (*func)(double*, int), double alpha_lo, double alpha_hi, double* x, double*p,  int length){
+  double phi_new, alpha, c1, c2, grad_phi;
+  c1 = 1; // modify
+  c2 = 1; // modify
+  while(1){
+    // Obtain alpha
+    alpha   = (alpha_lo + alpha_hi)/2; // modify
+    phi_new = phi(func, x, p, alpha, length);
+    if((phi_new > func(x, length) + c1*alpha*dotProd(gradCentralDiff(func, x, length), p, length)) || (phi_new >= phi(func, x, p, alpha_lo, length))){
+      alpha_hi = alpha;
+    }else{
+      grad_phi = dotProd(gradCentralDiff(func, vSum(x, vProd(p, alpha, length), length), length), p, length);
+      if(abs(grad_phi) <= -c2 * dotProd(gradCentralDiff(func, x, length), p, length))
+        return alpha;
+      if(grad_phi * (alpha_hi - alpha_lo))
+        alpha_hi = alpha_lo;
+      alpha_lo = alpha;
+    }
+  }
+}
+
 
 /* -------------------------------------
  * Método de búsqueda de línea para tamaño
@@ -113,10 +149,9 @@ double phi(double (*func)(double*, int), double* x, double* p, double alpha, int
  * res: Gradiente de fun evaluado en x.
  * -------------------------------------
  */
-/*double lineSearch(double (*func)(double*, int), double* x, double* p, int length){
-  // phi(a) = f(x + ap)
+double lineSearch(double (*func)(double*, int), double* x, double* p, int length){
   // Declare variables.
-  double alpha_old, alphaM, alpha_new, phi_new, phi_old, c1, c2;
+  double alpha_old, alphaM, alpha_new, phi_new, phi_old, c1, c2, grad_phi, alpha_aux;
   int i;
   // Initialize variables.
   alpha_old = 0;
@@ -124,13 +159,50 @@ double phi(double (*func)(double*, int), double* x, double* p, double alpha, int
   alpha_new = rand() % ((int) alphaM);
   i  = 0;
   c1 = 1;  // Modify
+  c2 = 1;  // Modify
   while(1){
     phi_new = phi(func, x, p, alpha_new, length);
     if((phi_new > func(x, length) + dotProd(gradCentralDiff(func, x, length), p, length) * (c1 * alpha_new)) ||
-       ((phi_new > phi_old) && i > 1)){
-      return zoom(alpha_old, alpha_new);
-    }
-    if(dotProd(gradCentralDiff(func, vSum(x,vProd(p, alpha_new, length), length), length), p, length))
+       ((phi_new >= phi_old) && i > 1))
+      return zoom(func, alpha_old, alpha_new, x, p, length);
+    grad_phi = dotProd(gradCentralDiff(func, vSum(x, vProd(p, alpha_new, length), length), length), p, length);
+    if(abs(grad_phi) <= -c2*dotProd(gradCentralDiff(func, x, length), p, length))
+      return alpha_new;
+    if(grad_phi >= 0)
+      return zoom(func, alpha_new, alpha_old, x, p, length);
+    alpha_aux = alpha_new;
+    alpha_new = alpha_new + (rand() % (int)(alpha_new - alpha_old));
+    alpha_old = alpha_aux;
   }
 }
-*/
+
+
+/* -------------------------------------
+ * Backtrack
+ * IN
+ * fun: Apuntador a una función que recibe
+ * un apuntador a un double y un entero.
+ * x: Apuntador a un vector sobre el cual
+ * se quiere aproximar el gradiente
+ * p: Una dirección de descenso.
+ * length: Longitud del vector.
+ * OUT
+ * res: Gradiente de fun evaluado en x.
+ * -------------------------------------
+ */
+double backTrack(double (*func)(double*, int), double* x, double* p, int length){
+  // Declare variables.
+  double *x_new;
+  double alpha, rho, c;
+  // Initialize variables.
+  alpha = 1; // Return alpha=1 whenever possible.
+  rho   = rand() % 1;
+  c     = rand() % 1;
+  // Iterate
+  x_new  = vSum(x, vProd(p, alpha, length), length);
+  while(func(x_new, length) <= (func(x, length) + dotProd(vProd(gradCentralDiff(func, x, length), c*alpha, length), p, length))){
+    x_new  = vSum(x, vProd(p, alpha, length), length);
+    alpha  = alpha * rho;
+  }
+  return alpha;
+}
